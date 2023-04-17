d9e03857ca12c475bb6729b736e3fe80a0ba7cd3
timlee789 <84083360+timlee789@users.noreply.github.com>
2023-04-17 09:48:41 -0400

    0417-3-detail modify

--- pages/video/[videoId].js
+++ pages/video/[videoId].js
@@ -3,60 +3,66 @@ import Link  from "next/link";
 import ReactPlayer from "react-player";
 import { Typography, Box, Stack } from "@mui/material";
 import { CheckCircle } from "@mui/icons-material";
-
-import { Videos} from '../../components/Videos';
+import { useRouter } from "next/router";
+import Videos from '../../components/Videos';
 import { fetchFromAPI } from "../../utils/fetchFromAPI";
+import Header from "@/components/Header";
 
 const VideoDetail = () => { 
   const [videoDetail, setVideoDetail] = useState(null);
   const [videos, setVideos] = useState(null);
-  //const {id} = useParams();
+  const { query } = useRouter();
+  const id = query.videoId;
+  console.log(id)
   
-  // useEffect(() => {
-  //   fetchFromAPI(`videos?part=snippet,statistics&id=${id}`)
-  //   .then((data) => setVideoDetail(data.items[0]));
+  useEffect(() => {
+    fetchFromAPI(`videos?part=snippet,statistics&id=${id}`)
+    .then((data) => setVideoDetail(data.items[0]));
 
-  //   fetchFromAPI(`search?part=snippet&relatedToVideoId=${id}&type=video`)
-  //   .then((data) => setVideos(data.items))
-  // }, [id]);
+    fetchFromAPI(`search?part=snippet&relatedToVideoId=${id}&type=video`)
+    .then((data) => setVideos(data.items))
+  }, [id]);
   if(!videoDetail?.snippet) return 'Loading...';
   const { snippet: {title, channelId, channelTitle}, statistics: { viewCount, likeCount} } = videoDetail
   console.log(videoDetail)
   return (
     <div>
-      <div>
-        <div>
-          <div>
+      <Header />
+    <Box minHeight='95vh'>
+      <Stack direction={{xs: 'column', md: 'row'}}>
+        <Box flex={1}>
+          <Box sx={{width: '100%', position: 'sticky', top: '86px'}}>
             <ReactPlayer
                 url={`https://www.youtube.com/watch?v=${id}`}
                 className = 'react-player'
                 controls
                 />
-            <p>
+            <Typography color='#000' variant="h5" frontWeight='bold' p={2}>
                {title} {/* {videoDetail.snippet.title} */}
-            </p>
-            <div>
+            </Typography>
+            <Stack direction='row' justifyContent='space-between' sx={{color: '#000'}} py={1} px={2}>
               <Link href={`/channel/${channelId}`}>
-                <p>
+                <Typography variant={{ sm: 'subtitle1', md: 'h6'}} color='#000'>
                   {channelTitle}
                   <CheckCircle sx={{fontSize: '12px', color: 'gray', ml: '5px'}} />
-                </p>
+                </Typography>
               </Link>
-            <div>
-                <p>
+            <Stack direction='row' gap='20px' alignItems='center'>
+                <Typography variant="body1" sx={{opacity: 0.7}}>
                   {parseInt(viewCount).toLocaleString()} views
-                </p>
-                <p>
+                </Typography>
+                <Typography variant="body1" sx={{opacity: 0.7}}>
                   {parseInt(likeCount).toLocaleString()} likes
-                </p>
-            </div>
-            </div>
-          </div>
-        </div>
+                </Typography>
+            </Stack>
+            </Stack>
+          </Box>
+        </Box>
         <Box px={2} py={{md: 1, xs: 5}} justifyContent='center' alignItems='center'>
         <Videos videos={videos} direction='column' />
          </Box>
-      </div>
+      </Stack>
+    </Box>
     </div>
   )
 }
